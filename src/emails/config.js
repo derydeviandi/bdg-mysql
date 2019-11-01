aef442a8e43d03c1a4839b46ea1eeb1fa25faacd
dery <derydeviandi@gmail.com>
2019-11-01 10:49:13 +0700

    kirim email

---
+++ src/emails/config.js
@@ -0,0 +1,6 @@
+let emailconfig = {
+    clientId: '1035268968639-4cra4ia1p7mi4iqs892cf52h4qv4q7rr.apps.googleusercontent.com',
+    clientSecret: 'mpsen416eIMHKXqmFILJBl68',
+    refreshToken: '1//04PK7Wr8-ARp6CgYIARAAGAQSNwF-L9Ir8qp86oUpD1Yj8nP2UBYa9Nh784IzhBG7VJ8rulfTW0bKIZStiaUu4qyIqDyJSsanSoQ'
+}
+module.exports = emailconfig
---
+++ src/emails/nodemailer.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer')
+const emailconfig = require('./config')
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: 'derydeviandi@gmail.com',
+        clientId: emailconfig.clientId,
+        clientSecret: emailconfig.clientSecret,
+        refreshToken: emailconfig.refreshToken
+
+    }
+})
+
+let mail = {
+    from: 'Dery Dev <derydeviandi@gmail.com>',
+    to: 'derydeviandi@gmail.com',
+    subject: 'Selamat Datang',
+    html: `<h1>Hello Lads</h1>`
+}
+
+transporter.sendMail(mail, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('Email berhasil dikirim')
+})
