2a86ab1942d891133130cba148a7150a5bd3f4e5
dery <derydeviandi@gmail.com>
2019-11-06 10:02:10 +0700

    fix index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 
 const userRouter = require('./routers/userRouter')
 const taskRouter = require('./routers/taskRouter')
 
 const app = express()
-const port = 2019
+const port = process.env.PORT || 2019
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Running at ${port}</h1>`)
+})
 
 app.listen(port, () => {
     console.log(`Running at ${port}`)
